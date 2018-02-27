1d88851041a4291dcde59a501161b1667ff9811d
oloapinivad <p.davini@isac.cnr.it>
2018-02-27 22:30:30 +0100

    introducing autohc.sh

---
+++ script/autohc.sh
@@ -0,0 +1,122 @@
+#!/usr/bin/env bash
+
+usage()
+{
+   echo "Usage:"
+   echo "       hc.sh [-a account] [-r rundir] [-m months_per_leg] EXP"
+   echo
+   echo "Submit to a job scheduler an HIRESCLIM2 postprocessing of experiment EXP"
+   echo " (started in YREF) from YEAR1 to YEAR2. For each year, the script makes a"
+   echo " wrapper around master_hiresclim.sh, and submit it through the job scheduler."
+   echo
+   echo "Submitted scripts and logs are in $SCRATCH/tmp_ecearth3"
+   echo
+   echo "Options are:"
+   echo "   -a ACCOUNT  : specify a different special project for accounting (default: ${ECE3_POSTPROC_ACCOUNT:-unknown})"
+   echo "   -r RUNDIR   : fully qualified path to another user EC-Earth RUNDIR"
+   echo "                   that is RUNDIR/EXP/output must exists and be readable (default output structure assumed)"
+   echo "   -m months_per_leg : run was performed with months_per_leg legs (yearly legs expected by default)"
+   echo "   -n numprocs       : set number of processors to use (default is 12)"
+}
+
+set -ue
+
+# -- default options
+account="${ECE3_POSTPROC_ACCOUNT-}"
+ALT_RUNDIR=""
+checkit=0
+options=""
+nprocs=12
+yref=""
+
+# -- options
+while getopts "hr:a:m:n:" opt; do
+    case "$opt" in
+        h)
+            usage
+            exit 0
+            ;;
+        n)  nprocs=$OPTARG
+            ;;
+        m)  options=${options}" -m $OPTARG"
+            ;;
+        r)  options=${options}" -r '$OPTARG'"
+            ALT_RUNDIR="$OPTARG"
+            ;;
+        a)  account=$OPTARG
+            ;;
+        *)  usage
+            exit 1
+    esac
+done
+shift $((OPTIND-1))
+
+if [ $# -ne 1 ]; then
+   usage
+   exit 1
+fi
+
+EXPID=$1
+
+# check environment
+[[ -z "${ECE3_POSTPROC_TOPDIR:-}" ]] && echo "User environment not set. See ../README." && exit 1
+. ${ECE3_POSTPROC_TOPDIR}/functions.sh
+check_environment
+
+# -- get submit command
+CONFDIR=${ECE3_POSTPROC_TOPDIR}/conf/${ECE3_POSTPROC_MACHINE}
+
+. ${CONFDIR}/conf_hiresclim_${ECE3_POSTPROC_MACHINE}.sh
+
+if [[ -n $ALT_RUNDIR ]]
+then
+    export IFSRESULTS0=$ALT_RUNDIR/${COREIFSDIR}
+fi
+
+# Find first and last year
+year="*"
+YEAR_LAST=$( basename $(eval echo $IFSRESULTS0/ICMSH${EXPID}+????12 | rev | cut -f1 -d" " | rev) | cut -c11-14)
+YEAR_ZERO=$( basename $(eval echo $IFSRESULTS0/ICMSH${EXPID}+????12 | cut -f1 -d" ") | cut -c11-14)
+echo "First year is ${YEAR_ZERO}"
+echo "Last year is ${YEAR_LAST}"
+
+# take yref from YEAR_ZERO
+yref=${YEAR_ZERO}
+
+# -- Scratch dir (location of submit script and its log, and temporary files)
+OUT=$SCRATCH/tmp_ecearth3
+mkdir -p $OUT/log
+
+# -- Write and submit one script per year
+for YEAR in $(seq ${YEAR_ZERO} ${YEAR_LAST})
+do 
+    echo; echo ---- $YEAR -----
+    if [ -f  $(eval echo ${ECE3_POSTPROC_POSTDIR})/postcheck_$EXPID_$YEAR.txt ] 
+    then
+	cat $(eval echo ${ECE3_POSTPROC_POSTDIR})/postcheck_$EXPID_$YEAR.txt
+    else
+	# check if postproc is already going on (only for slurm)
+	ll=$(echo $(squeue -u $USER  -o %.16j | grep "hc_${EXPID}_${YEAR}"))
+	if [[ ! -z $ll ]] ; then
+            echo "Hiresclim postprocessing for year $YEAR already going on..."
+            continue
+        fi
+	
+	tgt_script=$OUT/hc_${EXPID}_${YEAR}.job
+        sed "s/<EXPID>/$1/" < ${CONFDIR}/hc_$ECE3_POSTPROC_MACHINE.tmpl > $tgt_script
+
+        [[ -n $account ]] && \
+        sed -i "s/<ACCOUNT>/$account/" $tgt_script || \
+        sed -i "/<ACCOUNT>/ d" $tgt_script
+
+        # -- number of processors to use, default 12
+        sed -i "s/<NPROCS>/$nprocs/" $tgt_script
+
+        sed -i "s/<YEAR>/$YEAR/" $tgt_script
+        sed -i "s|<YREF>|$yref|" $tgt_script
+        sed -i "s|<OUT>|$OUT|" $tgt_script
+        sed -i "s|<OPTIONS>|${options}|" $tgt_script
+	echo "Submitting for year $YEAR"
+        ${submit_cmd} $tgt_script
+    fi
+done
