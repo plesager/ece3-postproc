7d8c7b8a7ea66574b8186f90c8ff41d27347571b
plesager <sager@knmi.nl>
2019-07-08 13:26:29 +0000

    do not check on input data existence for timeseries, when it depends on another job

--- script/ts.sh
+++ script/ts.sh
@@ -79,7 +79,7 @@ then
 else
     INDATA=`eval echo ${ECE3_POSTPROC_POSTDIR}`/mon
 fi
-[[ ! -d $INDATA ]] && echo "*EE* Experiment HiresClim2 output dir $INDATA does not exist!" && exit 1
+[[ -z $dependency ]] && [[ ! -d $INDATA ]] && echo "*EE* Experiment HiresClim2 output dir $INDATA does not exist!" && exit 1
 
 
 # -- check previously computed TS
